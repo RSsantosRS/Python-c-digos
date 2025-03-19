02ece7b2ca811e42d1200af1e281d90bc05cfed5
rafael dantas <rafaeldantas1109@gmail.com>
2025-03-19 19:38:06 -0300

    Create classe com função.py

---
+++ Prog.Rapida.PY/classe com função.py
@@ -0,0 +1,6 @@
+class Aula_Hoje:
+    def exemplo_01():
+        texto = "Meu nome é Rafael Dantas e estou manipulando String"
+        print(texto[0:20:2])
+    if __name__ == "__main__":
+        exemplo_01()
