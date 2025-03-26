fbfb38cc3852cd7b7a621d33e926541f697065dc
rafael dantas <rafaeldantas1109@gmail.com>
2025-03-26 17:31:06 -0300

    Create importação_funcao_outro_arquivo.py

---
+++ testes/importação_funcao_outro_arquivo.py
@@ -0,0 +1,11 @@
+import teste
+
+teste.funcao02()
+#deve ser o nome do arquivo + . + o nome da funcao main executavel do arquivo + ()
+
+def funcao() -> None:
+    print ("buuuuu")
+
+if __name__ == "__main__":
+    funcao()
+    
